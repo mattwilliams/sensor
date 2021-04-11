f58f4ac7246b171423cdc0d8a9b126f37a67b4ee
Matthew <matthew@Matthew-Air.local>
2021-04-11 15:38:14 -0500

    updated to play audio

--- gpio.py
+++ gpio.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 from time import sleep
+import urllib.request
+import urllib.parse
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 17
@@ -13,6 +15,11 @@ try:
 	while True:
 		if GPIO.input(PIR_PIN):
 			print("Motion Detected!")
+
+			url = 'http://192.168.1.3:3000/audio?firstname=tony&lastname=toon'
+			f = urllib.request.urlopen(url)
+			print(f.read().decode('utf-8'))
+			
 		time.sleep(1)
 
 except KeyboardInterrupt:
